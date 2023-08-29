93d51692724573cfcb019dfbb517a9a29be828bd
Jose Rodriguez <jose.rodriguez.sanmartin@gmail.com>
2023-08-29 11:52:20 +0200

    creating user and seed

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  USER = ["supplier", "brand"]
+  validates :user_type, inclusion: { in: USER }
 end
