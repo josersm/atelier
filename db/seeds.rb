2d4468b7b333d05c1b0a6b60e696b6d0d4c07667
akira onishi <akira.frlnc@gmail.com>
2023-08-30 12:32:17 +0200

    seed updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,11 +10,22 @@ user_1 = User.create(
 )
 
 user_2 = User.create(
-  email: "oporto.manu@gmail.com",
+	email: "oporto.manu@gmail.com",
   password: "123456",
   user_type: "supplier"
 )
 
+user_3 = User.create(
+	email: "coporto.manu@gmail.com",
+  password: "1234567",
+  user_type: "supplier"
+)
+
+user_4 = User.create(
+	email: "camali@amali.com",
+	password: "1234567",
+	user_type: "brand"
+)
 
 Supplier.create(
   name: "Oporto manufacturing",
@@ -29,6 +40,19 @@ Supplier.create(
 	user: user_2
 )
 
+Supplier.create(
+  name: "Oporto manufacturing",
+  address: "Oporto",
+  description: "lalallala",
+  country: "Portugal",
+  email: "oporto.manu@gmail.com",
+  phone_number: 123456789,
+  price_rating: "€€€",
+  sustainability_rating: "8.7",
+  minimum_quantity: 150,
+	user: user_3
+)
+
 
 Brand.create(
   company_name: "Amali",
@@ -38,6 +62,16 @@ Brand.create(
   country: "Spain",
   address: "Modesto Lafuente",
 		user: user_1
+)
+
+Brand.create(
+  company_name: "Amali",
+  bic_number: "123456789",
+  email: "amali@amali.com",
+  contact_number: 123456789,
+  country: "Spain",
+  address: "Modesto Lafuente",
+		user: user_4
 )
 
 Favorite.create(
