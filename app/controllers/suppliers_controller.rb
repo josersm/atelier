e0975772553190942f394894a05c4e7dc41df0f7
William Muhirwa <muhirwawilliam245@gmail.com>
2023-08-31 15:40:08 +0200

    add map

--- app/controllers/suppliers_controller.rb
+++ app/controllers/suppliers_controller.rb
@@ -2,13 +2,7 @@ class SuppliersController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
   def index
-<<<<<<< HEAD
 
-    @suppliers = policy_scope(Supplier)
-
-    @suppliers = Supplier.all
-
-=======
     @countries = Supplier.distinct.pluck(:country)
     @min_value = Supplier.minimum(:minimum_quantity)
     @max_value = Supplier.maximum(:minimum_quantity)
@@ -30,7 +24,6 @@ class SuppliersController < ApplicationController
     if params[:minimum_quantity].present? && params[:max_quantity].present?
       @suppliers = @suppliers.where("minimum_quantity BETWEEN ? AND ?", params[:minimum_quantity], params[:max_quantity])
     end
->>>>>>> 3f0aa38cb45175836e01f2309796e523c306e291
   end
 
   def show
