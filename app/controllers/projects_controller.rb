54cd54161a8b936445da689bce25516d9493c11e
William Muhirwa <muhirwawilliam245@gmail.com>
2023-09-01 12:55:21 +0200

    past links to project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,7 @@
 class ProjectsController < ApplicationController
   def new
     @project = Project.new
+    @supplier = Supplier.find(params[:supplier_id])
     authorize @project
   end
 
@@ -10,10 +11,14 @@ class ProjectsController < ApplicationController
     @project.brand = @brand
     authorize @project
     if @project.save
-      redirect_to project_path(@project), notice: "Form was successfully created."
+      redirect_to dashboard_path(@project), notice: "Form was successfully created."
     else
       render :new
     end
+  end
+
+  def update
+    @project = Project.find(params[:id])
   end
 
   private
