cbd64d233db9252649099a9fbdcd0f4dcae087cc
gianluca cattin <gcattin97@gmail.com>
2023-09-03 16:16:39 +0200

    finished the user inbox

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,14 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :home
-
+  before_action :authenticate_user!, only: [:dashboard, :inbox]
   def home
     @suppliers = Supplier.all
   end
 
   def dashboard
 		@user = current_user
+  end
+
+  def inbox
+    @chatrooms = current_user.chatrooms
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   root to: "pages#home"
   get "/about_us", to: "pages#about_us"
   get "/dashboard", to: "pages#dashboard"
-
+  get "/inbox", to: "pages#inbox", as: :inbox
 
   resources :brands, only: [:new, :create, :edit, :update, :destroy] do
     resources :projects, only: [:index]
