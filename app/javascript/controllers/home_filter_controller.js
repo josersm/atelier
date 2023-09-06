f13b4c7d6861a12808deb3c84201e5e2024e2d5b
Jose Rodriguez <jose.rodriguez.sanmartin@gmail.com>
2023-09-06 12:17:00 +0200

    updating with master

--- app/javascript/controllers/home_filter_controller.js
+++ app/javascript/controllers/home_filter_controller.js
@@ -5,23 +5,24 @@ export default class extends Controller {
   connect() {
     console.log("Hello, Stimulus!", this.element)
   }
-  openModal() {
-    console.log("open modal")
-    this.modalElement.classList.add('is-open')
-  }
 
-  closeModal() {
-    this.modalElement.classList.remove('is-open')
-  }
+  // openModal() {
+  //   console.log("open modal")
+  //   this.modalElement.classList.add('is-open')
+  // }
 
-  get modalElement() {
-    return this.element.querySelector('.filter-modal');
-  }
+  // closeModal() {
+  //   this.modalElement.classList.remove('is-open')
+  // }
+
+  // get modalElement() {
+  //   return this.element.querySelector('.filter-modal');
+  // }
 
   showOption(event) {
     console.log('showOption called');
-    const filterType = event.currentTarget.getAttribute('data-home-filter')
-    const filterElement = this.element.querySelector(`[data-home-filter-toggle="${filterType}"]`)
+    const filterType = event.currentTarget.getAttribute('data-home-filter') // This part extracts the data-home-filter attribute from the clicked element, basically it takes the name of the attribute, for example country, to then added in the url and filter the elements
+    const filterElement = this.element.querySelector(`[data-home-filter-toggle="${filterType}"]`) //
     if (filterElement.classList.contains('is-shown')) {
       filterElement.classList.remove('is-shown');
     } else {
