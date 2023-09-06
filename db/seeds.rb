0405b3ebe7819b8ecbb02c7f676f14b75e13d79a
Jose Rodriguez <jose.rodriguez.sanmartin@gmail.com>
2023-09-06 17:44:26 +0200

    seed wip

--- db/seeds.rb
+++ db/seeds.rb
@@ -241,6 +241,575 @@ Supplier.create(
 
 puts "Suppliers have been created"
 
+# ------------------------------ SEEDS FOR FINAL DEMO ---------------------------------------------------------
+
+# The brand we will use for the demo
+
+
+# For the demo we will have suppliers from 8 different countries
+
+  # A. - SPAIN SUPPLIERS ----------
+
+#------------------------TEMPLATE: ERRASE WHEN DONE------------------------------
+# user_supplier_spain_1
+user_supplier_country_1 = ""
+user_supplier_name_1 = ""
+user_supplier_address_1 = ""
+user_supplier_description_1 = ""
+user_supplier_price_1 = "€€€"
+user_supplier_sus_1 = rand(3..5)
+
+user_supplier_1 = User.create(
+  email: "#{user_supplier_name_1}@gmail.com",
+  password: "123456",
+  user_type: "supplier",
+)
+
+Supplier.create(
+  name: "#{user_supplier_name_1}",
+  address: "#{user_supplier_address_1}",
+  description: "#{user_supplier_description_1}",
+  country: "#{user_supplier_country_1}",
+  email: "#{user_supplier_name_1}@gmail.com",
+  phone_number: Faker::PhoneNumber.cell_phone,
+  price_rating: "#{user_supplier_price_1}",
+  sustainability_rating: "#{user_supplier_sus_1}",
+  minimum_quantity: rand(1..300),
+  user: user_supplier_1
+)
+
+# ---------------------------------------------------------------------------
+
+# user_supplier_spain_1
+user_supplier_country_1 = "Spain"
+user_supplier_name_1 = "ModaGalicia"
+user_supplier_address_1 = "McDonald's, Polígono Industrial de La Grela, Rúa Severo Ochoa, 6, 15008 A Coruña"
+user_supplier_description_1 = Faker::Lorem.paragraph(sentence_count: 10)
+user_supplier_price_1 = "€€€"
+user_supplier_sus_1 = rand(3..5)
+
+user_supplier_1 = User.create(
+  email: "#{user_supplier_name_1}@gmail.com",
+  password: "123456",
+  user_type: "supplier",
+)
+
+Supplier.create(
+  name: "#{user_supplier_name_1}",
+  address: "#{user_supplier_address_1}",
+  description: "#{user_supplier_description_1}",
+  country: "#{user_supplier_country_1}",
+  email: "#{user_supplier_name_1}@gmail.com",
+  phone_number: Faker::PhoneNumber.cell_phone,
+  price_rating: "#{user_supplier_price_1}",
+  sustainability_rating: "#{user_supplier_sus_1}",
+  minimum_quantity: rand(1..300),
+  user: user_supplier_1
+)
+
+#-------------------------------------------------------------
+
+# user_supplier_spain_2
+user_supplier_country_2 = "Spain"
+user_supplier_name_2 = "TelasBarcelona"
+user_supplier_address_2 = "Av. Diagonal, 661, 08028 Barcelona"
+user_supplier_description_2 = Faker::Lorem.paragraph(sentence_count: 10)
+user_supplier_price_2 = "€€€"
+user_supplier_sus_2 = rand(3..5)
+
+user_supplier_2 = User.create(
+  email: "#{user_supplier_name_2}@gmail.com",
+  password: "123456",
+  user_type: "supplier",
+)
+
+Supplier.create(
+  name: "#{user_supplier_name_2}",
+  address: "#{user_supplier_address_2}",
+  description: "#{user_supplier_description_2}",
+  country: "#{user_supplier_country_2}",
+  email: "#{user_supplier_name_2}@gmail.com",
+  phone_number: Faker::PhoneNumber.cell_phone,
+  price_rating: "#{user_supplier_price_2}",
+  sustainability_rating: "#{user_supplier_sus_2}",
+  minimum_quantity: rand(1..300),
+  user: user_supplier_2
+)
+
+
+#------------------------------------------------------
+# user_supplier_spain_3
+user_supplier_country_3 = "Spain"
+user_supplier_name_3 = "TextilIbérica"
+user_supplier_address_3 = "Canal, 23, 15650 Cambre, A Coruña"
+user_supplier_description_3 = Faker::Lorem.paragraph(sentence_count: 10)
+user_supplier_price_3 = "€€€"
+user_supplier_sus_3 = rand(3..5)
+
+user_supplier_3 = User.create(
+  email: "#{user_supplier_name_3}@gmail.com",
+  password: "123456",
+  user_type: "supplier",
+)
+
+Supplier.create(
+  name: "#{user_supplier_name_3}",
+  address: "#{user_supplier_address_3}",
+  description: "#{user_supplier_description_3}",
+  country: "#{user_supplier_country_3}",
+  email: "#{user_supplier_name_3}@gmail.com",
+  phone_number: Faker::PhoneNumber.cell_phone,
+  price_rating: "#{user_supplier_price_3}",
+  sustainability_rating: "#{user_supplier_sus_3}",
+  minimum_quantity: rand(1..300),
+  user: user_supplier_3
+)
+
+# ---------------------------------------------------------------------------
+
+# user_supplier_spain_4
+user_supplier_country_4 = "Spain"
+user_supplier_name_4 = ""
+user_supplier_address_4 = ""
+user_supplier_description_4 = Faker::Lorem.paragraph(sentence_count: 10)
+user_supplier_price_4 = "€€€"
+user_supplier_sus_4 = rand(3..5)
+
+user_supplier_4 = User.create(
+  email: "#{user_supplier_name_4}@gmail.com",
+  password: "123456",
+  user_type: "supplier",
+)
+
+Supplier.create(
+  name: "#{user_supplier_name_4}",
+  address: "#{user_supplier_address_4}",
+  description: "#{user_supplier_description_4}",
+  country: "#{user_supplier_country_4}",
+  email: "#{user_supplier_name_4}@gmail.com",
+  phone_number: Faker::PhoneNumber.cell_phone,
+  price_rating: "#{user_supplier_price_4}",
+  sustainability_rating: "#{user_supplier_sus_4}",
+  minimum_quantity: rand(1..300),
+  user: user_supplier_4
+)
+
+# ---------------------------------------------------------------------------
+
+# user_supplier_spain_5
+user_supplier_country_5 = "Spain"
+user_supplier_name_5 = ""
+user_supplier_address_5 = ""
+user_supplier_description_5 = Faker::Lorem.paragraph(sentence_count: 10)
+user_supplier_price_5 = "€€€"
+user_supplier_sus_5 = rand(3..5)
+
+user_supplier_5 = User.create(
+  email: "#{user_supplier_name_5}@gmail.com",
+  password: "123456",
+  user_type: "supplier",
+)
+
+Supplier.create(
+  name: "#{user_supplier_name_5}",
+  address: "#{user_supplier_address_5}",
+  description: "#{user_supplier_description_5}",
+  country: "#{user_supplier_country_5}",
+  email: "#{user_supplier_name_5}@gmail.com",
+  phone_number: Faker::PhoneNumber.cell_phone,
+  price_rating: "#{user_supplier_price_5}",
+  sustainability_rating: "#{user_supplier_sus_5}",
+  minimum_quantity: rand(1..300),
+  user: user_supplier_5
+)
+
+# ---------------------------------------------------------------------------
+
+# user_supplier_spain_6
+user_supplier_country_6 = "Spain"
+user_supplier_name_6 = ""
+user_supplier_address_6 = ""
+user_supplier_description_6 = Faker::Lorem.paragraph(sentence_count: 10)
+user_supplier_price_6 = "€€€"
+user_supplier_sus_6 = rand(3..5)
+
+user_supplier_6 = User.create(
+  email: "#{user_supplier_name_6}@gmail.com",
+  password: "123456",
+  user_type: "supplier",
+)
+
+Supplier.create(
+  name: "#{user_supplier_name_6}",
+  address: "#{user_supplier_address_6}",
+  description: "#{user_supplier_description_6}",
+  country: "#{user_supplier_country_6}",
+  email: "#{user_supplier_name_6}@gmail.com",
+  phone_number: Faker::PhoneNumber.cell_phone,
+  price_rating: "#{user_supplier_price_6}",
+  sustainability_rating: "#{user_supplier_sus_6}",
+  minimum_quantity: rand(1..300),
+  user: user_supplier_6
+)
+
+# ---------------------------------------------------------------------------
+# user_supplier_spain_7
+user_supplier_country_7 = "Spain"
+user_supplier_name_7 = ""
+user_supplier_address_7 = ""
+user_supplier_description_7 = Faker::Lorem.paragraph(sentence_count: 10)
+user_supplier_price_7 = "€€€"
+user_supplier_sus_7 = rand(3..5)
+
+user_supplier_7 = User.create(
+  email: "#{user_supplier_name_7}@gmail.com",
+  password: "123456",
+  user_type: "supplier",
+)
+
+Supplier.create(
+  name: "#{user_supplier_name_7}",
+  address: "#{user_supplier_address_7}",
+  description: "#{user_supplier_description_7}",
+  country: "#{user_supplier_country_7}",
+  email: "#{user_supplier_name_7}@gmail.com",
+  phone_number: Faker::PhoneNumber.cell_phone,
+  price_rating: "#{user_supplier_price_7}",
+  sustainability_rating: "#{user_supplier_sus_7}",
+  minimum_quantity: rand(1..300),
+  user: user_supplier_7
+)
+
+# ---------------------------------------------------------------------------
+
+
+# user_supplier_spain_8
+user_supplier_country_8 = ""
+user_supplier_name_8 = ""
+user_supplier_address_8 = ""
+user_supplier_description_8 = Faker::Lorem.paragraph(sentence_count: 10)
+user_supplier_price_8 = "€€€"
+user_supplier_sus_8 = rand(3..5)
+
+user_supplier_8 = User.create(
+  email: "#{user_supplier_name_8}@gmail.com",
+  password: "123456",
+  user_type: "supplier",
+)
+
+Supplier.create(
+  name: "#{user_supplier_name_8}",
+  address: "#{user_supplier_address_8}",
+  description: "#{user_supplier_description_8}",
+  country: "#{user_supplier_country_8}",
+  email: "#{user_supplier_name_8}@gmail.com",
+  phone_number: Faker::PhoneNumber.cell_phone,
+  price_rating: "#{user_supplier_price_8}",
+  sustainability_rating: "#{user_supplier_sus_8}",
+  minimum_quantity: rand(1..300),
+  user: user_supplier_8
+)
+
+# ---------------------------------------------------------------------------
+# user_supplier_spain_9
+user_supplier_country_9 = ""
+user_supplier_name_9 = ""
+user_supplier_address_9 = ""
+user_supplier_description_9 = Faker::Lorem.paragraph(sentence_count: 10)
+user_supplier_price_9 = "€€€"
+user_supplier_sus_9 = rand(3..5)
+
+user_supplier_9 = User.create(
+  email: "#{user_supplier_name_9}@gmail.com",
+  password: "123456",
+  user_type: "supplier",
+)
+
+Supplier.create(
+  name: "#{user_supplier_name_9}",
+  address: "#{user_supplier_address_9}",
+  description: "#{user_supplier_description_9}",
+  country: "#{user_supplier_country_9}",
+  email: "#{user_supplier_name_9}@gmail.com",
+  phone_number: Faker::PhoneNumber.cell_phone,
+  price_rating: "#{user_supplier_price_9}",
+  sustainability_rating: "#{user_supplier_sus_9}",
+  minimum_quantity: rand(1..300),
+  user: user_supplier_9
+)
+
+# ---------------------------------------------------------------------------
+
+# user_supplier_spain_10
+user_supplier_country_10 = ""
+user_supplier_name_10 = ""
+user_supplier_address_10 = ""
+user_supplier_description_10 = Faker::Lorem.paragraph(sentence_count: 10)
+user_supplier_price_10 = "€€€"
+user_supplier_sus_10 = rand(3..5)
+
+user_supplier_10 = User.create(
+  email: "#{user_supplier_name_10}@gmail.com",
+  password: "123456",
+  user_type: "supplier",
+)
+
+Supplier.create(
+  name: "#{user_supplier_name_10}",
+  address: "#{user_supplier_address_10}",
+  description: "#{user_supplier_description_10}",
+  country: "#{user_supplier_country_10}",
+  email: "#{user_supplier_name_10}@gmail.com",
+  phone_number: Faker::PhoneNumber.cell_phone,
+  price_rating: "#{user_supplier_price_10}",
+  sustainability_rating: "#{user_supplier_sus_10}",
+  minimum_quantity: rand(1..300),
+  user: user_supplier_10
+)
+
+# ---------------------------------------------------------------------------
+
+# user_supplier_spain_11
+user_supplier_country_11 = ""
+user_supplier_name_11 = ""
+user_supplier_address_11 = ""
+user_supplier_description_11 = Faker::Lorem.paragraph(sentence_count: 10)
+user_supplier_price_11 = "€€€"
+user_supplier_sus_11 = rand(3..5)
+
+user_supplier_11 = User.create(
+  email: "#{user_supplier_name_11}@gmail.com",
+  password: "123456",
+  user_type: "supplier",
+)
+
+Supplier.create(
+  name: "#{user_supplier_name_11}",
+  address: "#{user_supplier_address_11}",
+  description: "#{user_supplier_description_11}",
+  country: "#{user_supplier_country_11}",
+  email: "#{user_supplier_name_11}@gmail.com",
+  phone_number: Faker::PhoneNumber.cell_phone,
+  price_rating: "#{user_supplier_price_11}",
+  sustainability_rating: "#{user_supplier_sus_11}",
+  minimum_quantity: rand(1..300),
+  user: user_supplier_11
+)
+
+# ---------------------------------------------------------------------------
+
+# user_supplier_spain_12
+user_supplier_country_12 = ""
+user_supplier_name_12 = ""
+user_supplier_address_12 = ""
+user_supplier_description_12 = Faker::Lorem.paragraph(sentence_count: 10)
+user_supplier_price_12 = "€€€"
+user_supplier_sus_12 = rand(3..5)
+
+user_supplier_12 = User.create(
+  email: "#{user_supplier_name_12}@gmail.com",
+  password: "123456",
+  user_type: "supplier",
+)
+
+Supplier.create(
+  name: "#{user_supplier_name_12}",
+  address: "#{user_supplier_address_12}",
+  description: "#{user_supplier_description_12}",
+  country: "#{user_supplier_country_12}",
+  email: "#{user_supplier_name_12}@gmail.com",
+  phone_number: Faker::PhoneNumber.cell_phone,
+  price_rating: "#{user_supplier_price_12}",
+  sustainability_rating: "#{user_supplier_sus_12}",
+  minimum_quantity: rand(1..300),
+  user: user_supplier_12
+)
+
+# ---------------------------------------------------------------------------
+
+
+# user_supplier_spain_13
+user_supplier_country_13 = ""
+user_supplier_name_13 = ""
+user_supplier_address_13 = ""
+user_supplier_description_13 = Faker::Lorem.paragraph(sentence_count: 10)
+user_supplier_price_13 = "€€€"
+user_supplier_sus_13 = rand(3..5)
+
+user_supplier_13 = User.create(
+  email: "#{user_supplier_name_13}@gmail.com",
+  password: "123456",
+  user_type: "supplier",
+)
+
+Supplier.create(
+  name: "#{user_supplier_name_13}",
+  address: "#{user_supplier_address_13}",
+  description: "#{user_supplier_description_13}",
+  country: "#{user_supplier_country_13}",
+  email: "#{user_supplier_name_13}@gmail.com",
+  phone_number: Faker::PhoneNumber.cell_phone,
+  price_rating: "#{user_supplier_price_13}",
+  sustainability_rating: "#{user_supplier_sus_13}",
+  minimum_quantity: rand(13..300),
+  user: user_supplier_13
+)
+
+# ---------------------------------------------------------------------------
+
+# user_supplier_spain_14
+user_supplier_country_14 = ""
+user_supplier_name_14 = ""
+user_supplier_address_14 = ""
+user_supplier_description_14 = Faker::Lorem.paragraph(sentence_count: 10)
+user_supplier_price_14 = "€€€"
+user_supplier_sus_14 = rand(3..5)
+
+user_supplier_14 = User.create(
+  email: "#{user_supplier_name_14}@gmail.com",
+  password: "123456",
+  user_type: "supplier",
+)
+
+Supplier.create(
+  name: "#{user_supplier_name_14}",
+  address: "#{user_supplier_address_14}",
+  description: "#{user_supplier_description_14}",
+  country: "#{user_supplier_country_14}",
+  email: "#{user_supplier_name_14}@gmail.com",
+  phone_number: Faker::PhoneNumber.cell_phone,
+  price_rating: "#{user_supplier_price_14}",
+  sustainability_rating: "#{user_supplier_sus_14}",
+  minimum_quantity: rand(14..300),
+  user: user_supplier_14
+)
+
+# ---------------------------------------------------------------------------
+
+# user_supplier_spain_15
+user_supplier_country_15 = ""
+user_supplier_name_15 = ""
+user_supplier_address_15 = ""
+user_supplier_description_15 = Faker::Lorem.paragraph(sentence_count: 10)
+user_supplier_price_15 = "€€€"
+user_supplier_sus_15 = rand(3..5)
+
+user_supplier_15 = User.create(
+  email: "#{user_supplier_name_15}@gmail.com",
+  password: "123456",
+  user_type: "supplier",
+)
+
+Supplier.create(
+  name: "#{user_supplier_name_15}",
+  address: "#{user_supplier_address_15}",
+  description: "#{user_supplier_description_15}",
+  country: "#{user_supplier_country_15}",
+  email: "#{user_supplier_name_15}@gmail.com",
+  phone_number: Faker::PhoneNumber.cell_phone,
+  price_rating: "#{user_supplier_price_15}",
+  sustainability_rating: "#{user_supplier_sus_15}",
+  minimum_quantity: rand(15..300),
+  user: user_supplier_15
+)
+
+# ---------------------------------------------------------------------------
+
+# user_supplier_spain_16
+user_supplier_country_16 = ""
+user_supplier_name_16 = ""
+user_supplier_address_16 = ""
+user_supplier_description_16 = Faker::Lorem.paragraph(sentence_count: 10)
+user_supplier_price_16 = "€€€"
+user_supplier_sus_16 = rand(3..5)
+
+user_supplier_16 = User.create(
+  email: "#{user_supplier_name_16}@gmail.com",
+  password: "123456",
+  user_type: "supplier",
+)
+
+Supplier.create(
+  name: "#{user_supplier_name_16}",
+  address: "#{user_supplier_address_16}",
+  description: "#{user_supplier_description_16}",
+  country: "#{user_supplier_country_16}",
+  email: "#{user_supplier_name_16}@gmail.com",
+  phone_number: Faker::PhoneNumber.cell_phone,
+  price_rating: "#{user_supplier_price_16}",
+  sustainability_rating: "#{user_supplier_sus_16}",
+  minimum_quantity: rand(16..300),
+  user: user_supplier_16
+)
+
+# ---------------------------------------------------------------------------
+
+# B. - PORTUGAL SUPPLIERS ----------
+
+# user_supplier_spain_17
+user_supplier_country_17 = ""
+user_supplier_name_17 = ""
+user_supplier_address_17 = ""
+user_supplier_description_17 = Faker::Lorem.paragraph(sentence_count: 10)
+user_supplier_price_17 = "€€€"
+user_supplier_sus_17 = rand(3..5)
+
+user_supplier_17 = User.create(
+  email: "#{user_supplier_name_17}@gmail.com",
+  password: "123456",
+  user_type: "supplier",
+)
+
+Supplier.create(
+  name: "#{user_supplier_name_17}",
+  address: "#{user_supplier_address_17}",
+  description: "#{user_supplier_description_17}",
+  country: "#{user_supplier_country_17}",
+  email: "#{user_supplier_name_17}@gmail.com",
+  phone_number: Faker::PhoneNumber.cell_phone,
+  price_rating: "#{user_supplier_price_17}",
+  sustainability_rating: "#{user_supplier_sus_17}",
+  minimum_quantity: rand(17..300),
+  user: user_supplier_17
+)
+
+# ---------------------------------------------------------------------------
+
+# user_supplier_spain_18
+user_supplier_country_18 = ""
+user_supplier_name_18 = ""
+user_supplier_address_18 = ""
+user_supplier_description_18 = Faker::Lorem.paragraph(sentence_count: 10)
+user_supplier_price_18 = "€€€"
+user_supplier_sus_18 = rand(3..5)
+
+user_supplier_18 = User.create(
+  email: "#{user_supplier_name_18}@gmail.com",
+  password: "123456",
+  user_type: "supplier",
+)
+
+Supplier.create(
+  name: "#{user_supplier_name_18}",
+  address: "#{user_supplier_address_18}",
+  description: "#{user_supplier_description_18}",
+  country: "#{user_supplier_country_18}",
+  email: "#{user_supplier_name_18}@gmail.com",
+  phone_number: Faker::PhoneNumber.cell_phone,
+  price_rating: "#{user_supplier_price_18}",
+  sustainability_rating: "#{user_supplier_sus_18}",
+  minimum_quantity: rand(18..300),
+  user: user_supplier_18
+)
+
+# ---------------------------------------------------------------------------
+
+
+
+
+
+
+
 
 # favorite
 
