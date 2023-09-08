b4e2c0da3f46ebe4a2d62165e04404ac8776c729
gianluca cattin <gcattin97@gmail.com>
2023-09-08 16:07:28 +0200

    fixing seeds

--- app/channels/chatroom_channel.rb
+++ app/channels/chatroom_channel.rb
@@ -1,9 +1,12 @@
 class ChatroomChannel < ApplicationCable::Channel
   def subscribed
-    chatroom = Chatroom.find(params[:chatroom_id])
-    stream_for chatroom
+    if params[:chatroom_id].present?
+      chatroom = Chatroom.find(params[:chatroom_id])
+      stream_for chatroom
+    else
+      reject
+    end
   end
-
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
   end
